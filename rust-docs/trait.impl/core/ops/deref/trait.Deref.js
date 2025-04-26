3b13a40a9eebe30a1be98a715a6e5839ac567ae8
nazar-pc <nazar@mokrynskyi.com>
2025-04-26 03:50:07 +0000

    Deploying to gh-pages from @ nazar-pc/abundance@bd168887a82d57d46b19ea105e4fa004e658c83d 🚀

---
+++ rust-docs/trait.impl/core/ops/deref/trait.Deref.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["ab_contracts_io_type",[["impl&lt;const CAPACITY: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/core/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"ab_contracts_io_type/fixed_capacity_string/struct.FixedCapacityStringU16.html\" title=\"struct ab_contracts_io_type::fixed_capacity_string::FixedCapacityStringU16\">FixedCapacityStringU16</a>&lt;CAPACITY&gt;"],["impl&lt;const CAPACITY: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/core/primitive.usize.html\">usize</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"ab_contracts_io_type/fixed_capacity_string/struct.FixedCapacityStringU8.html\" title=\"struct ab_contracts_io_type::fixed_capacity_string::FixedCapacityStringU8\">FixedCapacityStringU8</a>&lt;CAPACITY&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1057]}
